a032eba42d0564c336bdf2927bad33ad25bb8ac8
Oscar Almgren <oscar.almgren@gmail.com>
2023-05-15 16:16:57 +0200

    Checkarea for Triangle add

--- structs/structs.go
+++ structs/structs.go
@@ -2,13 +2,17 @@ package structs
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
-	Width  float64
-	Height float64
+	width  float64
+	height float64
 }
 
 func (r Rectangle) Area() (area float64) {
-	return r.Height * r.Width
+	return r.height * r.width
 }
 
 type Circle struct {
@@ -19,6 +23,15 @@ func (c Circle) Area() (area float64) {
 	return math.Pi * c.Radius * c.Radius
 }
 
+type Triangle struct {
+	base   float64
+	height float64
+}
+
+func (t Triangle) Area() (area float64) {
+	return (t.base * t.height) / 2
+}
+
 func Perimeter(r Rectangle) float64 {
-	return 2 * (r.Height + r.Width)
+	return 2 * (r.height + r.width)
 }
