3e342835e27558974f1bba2b9e33578782168e2f
Oscar Almgren <oscar.almgren@gmail.com>
2023-05-15 09:43:45 +0200

    Test area and perimiter with structs and method recievers

--- structs/structs.go
+++ structs/structs.go
@@ -1,1 +1,24 @@
 package structs
+
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() (area float64) {
+	return r.Height * r.Width
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() (area float64) {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func Perimeter(r Rectangle) float64 {
+	return 2 * (r.Height + r.Width)
+}
