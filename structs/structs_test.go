be770822d9aa857e251c20262f362c6ae48bd098
Oscar Almgren <oscar.almgren@gmail.com>
2023-05-16 15:42:24 +0200

    areaTest anonymous struct for Table Driven Tests

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -14,28 +14,48 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		// If you have this method baby, you're my type
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{width: 12, height: 6}, hasArea: 72},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{base: 12, height: 6}, hasArea: 36.0},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		checkArea(t, rectangle, 72.0)
-	})
+	for _, tt := range areaTests {
+		// using tt.name from the case to use it as the `t.Run`test name`
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
 
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	// checkArea := func(t testing.TB, shape Shape, want float64) {
+	// 	// If you have this method baby, you're my type
+	// 	t.Helper()
+	// 	got := shape.Area()
+	// 	if got != want {
+	// 		t.Errorf("got %g want %g", got, want)
+	// 	}
+	// }
 
-	t.Run("trinagles", func(t *testing.T) {
-		triangle := Triangle{4.0, 4.0}
-		checkArea(t, triangle, 8.0)
-	})
+	// t.Run("rectangles", func(t *testing.T) {
+	// 	rectangle := Rectangle{12.0, 6.0}
+	// 	checkArea(t, rectangle, 72.0)
+	// })
+
+	// t.Run("circles", func(t *testing.T) {
+	// 	circle := Circle{10}
+	// 	checkArea(t, circle, )
+	// })
+
+	// t.Run("trinagles", func(t *testing.T) {
+	// 	triangle := Triangle{4.0, 4.0}
+	// 	checkArea(t, triangle, 8.0)
+	// })
 
 }
