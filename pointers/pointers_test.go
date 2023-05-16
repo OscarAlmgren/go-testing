382392896ef1cea30826f25d381f48d049687b21
Oscar Almgren <oscar.almgren@gmail.com>
2023-05-16 15:42:38 +0200

    Pointers commit

---
+++ pointers/pointers_test.go
@@ -0,0 +1,38 @@
+package pointers
+
+import (
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+
+		wallet.Deposit(Bitcoin(10))
+
+		got := wallet.Balance()
+		want := Bitcoin(10)
+
+		// fmt.Printf("address of balance in Test is %v \n", &wallet.balance)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+}
